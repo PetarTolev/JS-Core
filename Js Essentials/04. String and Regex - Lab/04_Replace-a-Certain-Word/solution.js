75b1462a3806e1917eacbe472875d212675a566b
PetarTolev <petar.dtolev@gmail.com>
2019-07-30 21:05:36 +0300

    Added exercise

--- Js Essentials/04. String and Regex - Lab/04_Replace-a-Certain-Word/solution.js
+++ Js Essentials/04. String and Regex - Lab/04_Replace-a-Certain-Word/solution.js
@@ -1,4 +1,15 @@
 function solve() {
+    let wordElement = document.getElementById('word').value;
+    let textAreaElement = JSON.parse(document.getElementById('text').value);
+    let resultElement = document.getElementById('result');
 
-    //TODO...
+    let wordForReplace = textAreaElement[0].split(' ')[2];
+    
+    for (const line of textAreaElement) {
+        let a = line.replace(new RegExp(wordForReplace, 'i'), wordElement);
+        let p = document.createElement('p');
+        p.innerHTML = a;
+
+        resultElement.appendChild(p);
+    }
 }
