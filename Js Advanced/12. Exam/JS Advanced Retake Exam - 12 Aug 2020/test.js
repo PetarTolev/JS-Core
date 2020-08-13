a21baee16309bae585cad21a545c03c7afbfc67b
PetarTolev <petar.dtolev@gmail.com>
2020-08-13 18:32:13 +0300

    Add retake exam

---
+++ Js Advanced/12. Exam/JS Advanced Retake Exam - 12 Aug 2020/test.js
@@ -0,0 +1,15 @@
+// let a = new Dog();
+// a.toString();
+// class Dog {
+//     toString() {
+//         console.log('dog');
+//     }
+// }
+
+let r = new Rectangle();
+r.toString();
+const Rectangle = class {
+    toString() {
+        console.log('rec');
+    }
+};
